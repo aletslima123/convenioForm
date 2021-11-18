8f5583856c8ff784243b47e918b627708fba43f8
Alexandre <fornecedor.vlima@brasilprev.com>
2021-11-18 20:46:07 -0300

    icon list style, svgs, image, css fixes

--- convenio-form/src/polyfills.ts
+++ convenio-form/src/polyfills.ts
@@ -57,9 +57,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'core-js/es/array';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
